b57f08ce3c98732cf022eeee0e63796a5300f52d
JacksChoco <ljs8860@playauto.co.kr>
2018-06-17 20:55:11 +0900

    덱 초기화

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(card, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
