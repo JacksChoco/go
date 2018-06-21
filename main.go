bb9daa95a9910b8aa29ddae97e9fa6788819aba8
JacksChoco <ljs8860@playauto.co.kr>
2018-06-21 10:28:10 +0900

    struct 사용

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	fristName string
+	lastName  string
+	contact   contactInfo
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	jim := person{
+		fristName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "zkmm234@naver.com",
+			zipCode: 123,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).fristName = newFirstName
 }
